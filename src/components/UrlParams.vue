47930c8c54cdaf54596f56acb9989e541bd518cc
squarer <squarerliao@gmail.com>
2017-06-24 20:35:46 +0800

    skip tabbing on remove button

--- src/components/UrlParams.vue
+++ src/components/UrlParams.vue
@@ -3,7 +3,7 @@
     <div class="params-wrapper" v-for="(param, index) in params" :key="param">
       <input @keyup="isLast(index) ? addParams($event) : null" class="params-key" :placeholder="isLast(index) ? 'new key' : null" v-model="param.key">
       <input @keyup="isLast(index) ? addParams($event) : null" class="params-value" :placeholder="isLast(index) ? 'value' : null" v-model="param.value">
-      <button :style="isLast(index) ? { visibility: 'hidden' } : null" @click="remove(index)">
+      <button :style="isLast(index) ? { visibility: 'hidden' } : null" @click="remove(index)" tabindex="-1">
         <i>remove_circle_outline</i>
       </button>
     </div>
